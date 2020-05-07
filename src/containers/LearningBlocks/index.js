77edf795bc7a89ac26b568825a101eb6f3992f81
chandanucreate <chandankumar@ucreate.co.in>
2020-05-08 00:17:56 +0530

    Update the date format

--- src/containers/LearningBlocks/index.js
+++ src/containers/LearningBlocks/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import withStyles from 'react-jss';
 import ScrollAnimation from 'react-animate-on-scroll';
+import moment from 'moment';
 import { LearningBlock } from '../../components';
 import Wrapper from '../../components/Wrapper'
 import LearningBlocksStyle from './style';
@@ -11,14 +12,17 @@ const LearningBlocks = ({classes, ...props}) => {
   return (    
     <div className={classes.postBox}>
       <Wrapper justifyContent='space-between'>
-        {props.postData.map((item, index) => {          
+        {props.postData.map((item, index) => { 
+          console.log(item, 'DATA');  
+          const customFormat = moment(item.date).fromNow();
+          console.log(customFormat, 'DATE FORMAT')    
           return(
             <ScrollAnimation className={classes.postBlock} animateIn="fadeIn" key={index}>
               <LearningBlock
                 name={item.name}
                 title={item.heading}
                 content={item.content}
-                date={item.date}
+                date={customFormat}
               />
               <Link className={classes.linkColor} to='/UserComment'>Add Comment</Link>
             </ScrollAnimation>
