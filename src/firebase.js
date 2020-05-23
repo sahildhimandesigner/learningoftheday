fb67a1c237ba36269c19651154a7f76a42e65571
inderpreet singh <inderpreetsingh@ucreate.co.in>
2020-05-24 01:46:54 +0530

    Updated the firebase.js file

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import firebase from 'firebase'
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: 'https://learningoftheday-3af1d.firebaseio.com',
+    databaseURL: "https://learningoftheday-3af1d.firebaseio.com",
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGE_SENDING_ID,
