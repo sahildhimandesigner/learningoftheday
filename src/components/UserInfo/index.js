6d91876f36366fd222e940d7dca4fd7cbaeba201
inderpreet singh <inderpreetsingh@ucreate.co.in>
2020-06-05 18:36:45 +0530

    Added level logic in the user profile

--- src/components/UserInfo/index.js
+++ src/components/UserInfo/index.js
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Headings } from '..'
 import UserAvtar from './../../images/user-placeholder.svg'  
 import UserStyle from './style'
 import withStyle from 'react-jss'
+import firebase from '../../firebase';
+import Level from '../../utilities/CoinsLevelMapper';
 
-const UserInfo = ({classes, ...props}) => { 
-    console.log(props.show, 'user info')
+const UserInfo = ({classes, ...props}) => {
+    const [coins, setCoins] = useState(0);
+
+    useEffect(() => {
+        const userInfo = firebase.database().ref(`users/${props.currentState.userId}`);
+        userInfo.on('value', function(snapshot){
+            setCoins(snapshot.val().coins ?? 0);
+        });
+    }, []);
     return (
         <div className={classes.userContainer}>      
             <img src={UserAvtar} alt={UserAvtar} />
-            <Headings as="h2">User Information</Headings>
+            <div className={classes.userInformation}>
+                <span>
+                    {`${props.currentState.firstName} ${props.currentState.lastName}`}
+                </span>
+                <span className={classes.levelInformation}><Level coins={coins}/></span>
+            </div>
+            
             <button onClick={() => props.click('allGoal')}>All Goal</button>
             <button onClick={() => props.click('addGoal')}>Add Goal</button>
         </div>
--- src/components/UserInfo/style.js
+++ src/components/UserInfo/style.js
@@ -5,6 +5,20 @@ const UserStyle = () => {
             flexFlow:'column',
             width: '40%',
             textAlign: 'center'
+        },
+        userInformation: {
+            display: 'flex',
+            justifyContent: 'space-evenly',
+            width: '50%',
+            paddingLeft: '32%',
+            paddingTop: '4%',
+            paddingBottom: '4%',
+            textTransform: 'capitalize'
+        },
+        levelInformation: {
+            fontSize: 12,
+            paddingTop: 4,
+            color: 'gray'
         }
     }
 }
